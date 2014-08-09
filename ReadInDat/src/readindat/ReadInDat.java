211bebdb01428173eefe40814c11c3249770c703
MDClark <m14.clark@connect.qut.edu.au>
2014-08-09 20:55:36 +1000

    Insert from flat file
    Implemented insertion from flat file into MySQL database. Still very
    rough.
    
    Database, table and data need to be dropped before runtime every time.
    Essentially hardcoded most aspects of the operation. Insertion operation
    takes over 1 minute on my PC.
    
    Look into batch insertions and prepared statements.

--- ReadInDat/src/readindat/ReadInDat.java
+++ ReadInDat/src/readindat/ReadInDat.java
@@ -8,6 +8,9 @@ package readindat;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,14 +18,27 @@ import java.io.IOException;
  */
 public class ReadInDat {
 
+    private static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/";
+    private static final String DB_NAME = "test";
+    private static final String USER = "root";
+    private static final String PASS = "password";
+
+    private static Statement statement;
+    private static ResultSet result;
+    private static Connection con;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         try {
-            readData("user_ratedmovies-timestamps.dat");
-        } catch (IOException e) {
-            e.printStackTrace();
+            CreateDatabase();
+            readData("user_taggedmovies.dat");
+        } catch (IOException ex) {
+            Logger.getLogger(ReadInDat.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
     }
 
@@ -32,19 +48,47 @@ public class ReadInDat {
      * @param location The location of the file to be read
      * @throws java.io.IOException
      */
-    public static void readData(String location) throws IOException {
+    public static void readData(String location) throws IOException, SQLException {
         BufferedReader reader = new BufferedReader(new FileReader(location));
         String line;
         
+        line = reader.readLine();
         while ((line = reader.readLine()) != null) {
             String[] parts = line.split("\t");
+            
+            statement = con.createStatement();
+            String sql = "INSERT INTO test.movie_tags(USER_ID, MOVIE_ID, TAG_ID)"
+                    + "VALUES (" + parts[0] + ", " + parts[1] + ", " + parts[2] + ")";
+            statement.executeUpdate(sql);
+        }
+    }
 
-            for (int i = 0; i < parts.length; i++) {
-                System.out.print(i + ":" + parts[i] + " ");
-                if (i == parts.length - 1) {
-                    System.out.println();
-                }
-            }
+    /**
+     * Create MySQL database and a table to store tag information from flat file
+     */
+    private static void CreateDatabase() {
+        try {
+            // Register JDBC driver
+            Class.forName(JDBC_DRIVER);
+
+            // Open a connection
+            con = DriverManager.getConnection(DB_URL, USER, PASS);
+
+            // Create the database
+            statement = con.createStatement();
+            String sql = "CREATE DATABASE test";
+            statement.executeUpdate(sql);
+
+            // Create the table(s)
+            statement = con.createStatement();
+            sql = "CREATE TABLE test.movie_tags "
+                    + "(USER_ID INTEGER NOT NULL, "
+                    + "MOVIE_ID INTEGER NOT NULL, "
+                    + "TAG_ID INTEGER NOT NULL, "
+                    + "PRIMARY KEY(USER_ID, MOVIE_ID, TAG_ID))";
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
